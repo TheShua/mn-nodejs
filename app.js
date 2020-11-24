daf942aecda5b517422b98179be2b2bdf8af4b76
Shua <g.deglisezinski@gmail.com>
2020-11-24 16:50:50 +0100

    Finale version

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+// Uncomment this to view the dev logs
+// const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -13,14 +13,14 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));
+// Uncomment this to view the dev logs
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios')
+let actualState = null;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  try {
+    axios.get("https://dev.fractal-it.fr:8443/fake_health_test?dynamic=true")
+      .then(result => {
+        // Init the actualState value with a new object if new or different status from actual
+        if (!actualState || result.data.status != actualState.status) {
+          actualState = { status: result.data.status, date: new Date() };
+        }
+        
+        // Calculate the seconds since last actualisation
+        const now = new Date();
+        const intervalInSeconds = Math.round(Math.abs((actualState.date.getTime() - now.getTime()) / 1000));
+        const time = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+        
+        if (actualState.status === "ok") {
+          // If status is "ok" and the log hasn't been sent yet
+          if (!actualState.log) {
+            console.log(`${time} : Status est ok`);
+            actualState.log = true;
+          }
+        } else {
+          // The status is "error" since 30sec and the log hasn't been sent yet
+          if(!actualState.log && intervalInSeconds >= 30) {
+            console.log(`${time} : Status est en erreur depuis plus de 30s`);
+            actualState.log = true;
+          }
+        }
+
+        // Render the result in the client
+        res.render('index', { status: actualState.status, seconds: intervalInSeconds });
+      })
+      .catch(err => res.send(err));
+  }
+  catch(err) {
+      console.error("Une erreur est survenue : ", err);
+  }
 });
+
 
 module.exports = router;
